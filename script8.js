0e7197801ae758a4d166d105fc229d312dbc6062
Pokmand <manudesj@msn.com>
2019-04-04 15:12:59 +0200

    exo 8 ok

---
+++ script8.js
@@ -0,0 +1,8 @@
+function savedataforms() {
+var age = document.getElementById("age").value;
+    if (age < 18) {
+      alert ("Vous êtes mineur");
+    } else {
+      alert ("Vous êtes majeur");
+    }
+  }
