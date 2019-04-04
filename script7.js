b09a768f4c4bfd400ecefa0ddd0a903a3682f311
Pokmand <manudesj@msn.com>
2019-04-04 14:26:44 +0200

    exo 7 ok

---
+++ script7.js
@@ -0,0 +1,6 @@
+function savedataforms() {
+  var shoeSize = document.getElementById("shoeSize").value;
+  var yearOfBirth = document.getElementById("yearOfBirth").value;
+  var result = ((shoeSize * 2 + 5) * 50 - yearOfBirth + 1769);
+  alert(result)
+}
